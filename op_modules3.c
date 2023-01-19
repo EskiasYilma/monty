c1a36f53bf6702ed90a2d15a5141180383cedc1e
GideonAmhaG <gideonamaha@gmail.com>
2023-01-19 19:26:52 +0000

    pstr, added 0 condition

--- op_modules3.c
+++ op_modules3.c
@@ -43,7 +43,7 @@ void pstr(stack_t **stack, unsigned int line_number)
 		return;
 	}
 	temp = *stack;
-	while (temp != NULL && temp->n >= 0 && temp->n <= 127)
+	while (temp != NULL && temp->n > 0 && temp->n <= 127)
 	{
 		printf("%c", temp->n);
 		temp = temp->next;
---
+++ x.m
@@ -0,0 +1,14 @@
+push 83
+push 111
+push 111
+push 111
+push 111
+push 0
+push 0 
+push 45
+push 111
+push 111
+push 40
+push 99
+push 83
+pstr
